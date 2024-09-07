477bf81fe90c737c9e4f148f6129cc2b2070959f
Abdul Wahid Muhaemin <wamuhabdul@gmail.com>
2024-09-07 17:15:46 +0700

    feat(models): add mass assignment attributes to BookingTransaction, CarService, CarStore, City, StorePhoto, and StoreService models

--- app/Models/CarStore.php
+++ app/Models/CarStore.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CarStore extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'is_open',
+        'is_full',
+        'city_id',
+        'thumbnail',
+        'address',
+        'phone_number',
+        'cs_name',
+    ];
 }
